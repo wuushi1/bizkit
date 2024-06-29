61798b631ea4d46b9bc5bc05b30fbd5155457166
wuushi1 <patricksurveys3@gmail.com>
2024-06-29 12:23:03 +0800

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -13,18 +13,43 @@ def match(match_id):
         return "Invalid match id", 404
 
     start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
+
+    # Sort asc
+    sorted1 = MATCHES[match_id][0]
+    sorted1.sort()
+
+    sorted2 = MATCHES[match_id][1]
+    sorted2.sort()
+    
+    msg = "Match found" if (is_match(sorted1, sorted2, 0, len(sorted1)-1)) else "Not found"
+
     end = time.time()
 
-    return {"message": msg, "elapsedTime": end - start}, 200
+    return {
+        "message": msg, 
+        "elapsedTime": end - start,
+    }, 200
 
+#function for matching
+def is_match(fave_numbers_1, fave_numbers_2, low, high):
+    found = False
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    
-    set_fave_numbers_1 = set(fave_numbers_1)
-    
     for number in fave_numbers_2:
-        if number not in set_fave_numbers_1:
-            return False
+        while low <= high: # check
+            mid = low + (high - low)//2
 
-    return True
+            if fave_numbers_1[mid] == number:
+                print(number)
+                found = True
+                break
+
+            elif fave_numbers_1[mid] < number:
+                low = mid+1
+
+            else:
+                high = mid-1
+
+        if found == False: 
+            break
+        
+    return found
